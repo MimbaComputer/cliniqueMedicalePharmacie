0498e15dd96bd6e422af4a566a0750e011d157bb
Mimba <baounabaouna@gmail.com>
2017-05-22 08:49:49 +0100

    Initialisation

--- src/main/java/org/mimba/bao/CliniqueMedicalePharmacieServiceApplication.java
+++ src/main/java/org/mimba/bao/CliniqueMedicalePharmacieServiceApplication.java
@@ -2,7 +2,10 @@ package org.mimba.bao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CliniqueMedicalePharmacieServiceApplication {
 
